9db024e43f97773281f2c95a2558cf1536d88479
Ablacodeur <agbolivicky@gmail.com>
2026-03-04 19:01:39 -0500

    ajout du loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import { Box, useMediaQuery } from "@mui/material";
+import CircleLoader from "react-spinners/CircleLoader";
 import "./App.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
   // ✅ Breakpoints simples (mobile / tablette / desktop)
   const isMobile = useMediaQuery("(max-width:600px)");
   const isTablet = useMediaQuery("(min-width:601px) and (max-width:1200px)");
@@ -11,8 +15,37 @@ function App() {
   const bgUrl = isMobile
     ? "/assets/projects/bg-small-2.png"
     : isTablet
-    ? "/assets/projects/bg-tablet.png" 
+    ? "/assets/projects/bg-tablet.png"
     : "/assets/projects/bg2.png";
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2 secondes 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // ✅ Loader plein écran
+  if (isLoading) {
+    return (
+      <Box
+        sx={{
+          minHeight: "100dvh",
+          width: "100%",
+          backgroundImage: `url(${bgUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CircleLoader color="#91B578" size={100} />
+      </Box>
+    );
+  }
 
   return (
     <Box
@@ -33,7 +66,7 @@ function App() {
       <Box
         sx={{
           flex: 1,
-          minHeight: 0, // ✅ CRUCIAL pour que le scroll marche en flex
+          minHeight: 0,
           overflowY: "auto",
           overflowX: "hidden",
           WebkitOverflowScrolling: "touch",
